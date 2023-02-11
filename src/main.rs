09b78fe28d74afea47779dbcb1146d0a1591fa01
BuildTools <t37.slime.1.1@gmail.com>
2023-02-10 21:53:47 -0300

    arreglos de la UI

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 #[macro_use] extern crate rocket;
 use rocket_dyn_templates::{Template, context};
 
+const HOSTNAME: &'static str = "localhost:8000";
+const PROTOCOL: &'static str = "http://";
+
 #[get("/")]
 fn index() -> Template {
    Template::render("index", context!{
-        hostname: "localhost:8000",
-        protocol: "http://"
+        hostname: HOSTNAME,
+        protocol: PROTOCOL
    }) 
 }
 
 #[get("/services")]
 fn services() -> Template {
     Template::render("services", context!{
-        hostname: "localhost:8000",
-        protocol: "http://"
+        hostname: HOSTNAME,
+        protocol: PROTOCOL
     })
 }
 
